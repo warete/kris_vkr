e5795eebeb8eeabf03dc6ca2ad97d705d31f06e1
Egor Borisovsky <egor@intervolga.ru>
2021-04-25 16:35:18 +0300

    комментарии

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ app = Flask(__name__)
 db_file_path = "vkr.db"
 
 
+# Функция для получения подключения к БД
 def get_conn(db_file_path):
     conn = sqlite3.connect(db_file_path)
     conn.row_factory = dict_factory
@@ -37,13 +38,17 @@ with get_conn(db_file_path) as conn:
     train_items = pd.DataFrame(cursor.fetchall())
     # Создаем модель и обучаем
     model = SVC(gamma='scale')
+    # x - температуры
     train_x = train_items[['t' + str(i) for i in range(13)]].values
+    # y - признак болен/здоров
     train_y = train_items['target']
+    # Обучаем через метод fit
     model.fit(train_x, train_y)
     # Посчитаем точность
     print(accuracy_score(train_y, model.predict(train_x)))
 
 
+# Функция для получения списка пациентов из БД
 def get_patients():
     with get_conn(db_file_path) as conn:
         cursor = conn.cursor()
@@ -51,25 +56,33 @@ def get_patients():
         return cursor.fetchall()
 
 
+# Главная страница
 @app.route('/')
 def index():
+    # Получаем список пациентов и пробрасываем на страницу
     patients = get_patients()
     return render_template('index.html', patients=patients)
 
 
+# Страница для обработки формы
 @app.route('/predict', methods=['POST'])
 def predict():
+    # Собираем всю информацию из формы
     patient = {'temps': {}, 'fio': request.form.get('fio'), 'birth_date': request.form.get('birth_date')}
     for i in range(13):
         patient['temps']['t' + str(i)] = request.form.get('t' + str(i))
 
+    # Если все что нам надо есть в форме (фио, дата рождения и точки не пустые), то пробуем определить диагноз
     if len(patient['fio']) and len(patient['birth_date']) and len(patient['temps']) == 13:
         with get_conn(db_file_path) as conn:
+            # Передаем в модель точки и получаем результат в виде числа (0 - здоров, 1 - болен)
             pred_data = model.predict([list(patient['temps'].values())])
             cursor = conn.cursor()
+            # Добавляем результат в бд. Одна строка в бд - одно обследование для пациентов
             cursor.execute('INSERT INTO patients (fio, birth_date, diagnosis, diagnosis_date) VALUES(?,?,?,?)',
                            (patient['fio'], patient['birth_date'], int(pred_data[0]), datetime.datetime.now()))
             conn.commit()
+            # Перенаправляем пользователя опять на главную страницу
             return redirect('/')
     else:
         return redirect('/')
